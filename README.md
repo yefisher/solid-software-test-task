2e9ddae31b5ec57ea5a8075d86e046bd0425f4ee
Yegor Fisher <egorfisher88@gmail.com>
2020-07-24 17:00:34 +0300

    Adding solution of the main task

---
+++ README.md
@@ -0,0 +1,1 @@
+"Solid Software flutter/dart test task" 
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  static const String _title = 'Solid Software test task';
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: const Text(_title)),
+          body: MyApp(),
+    ));
+  }
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _indexR;
+  int _indexG;
+  int _indexB;
+  double _opacity;
+
+  /*
+  * At the beginning the app starts with white background
+  * */
+  @override
+  void initState() {
+    super.initState();
+    _indexR = 255;
+    _indexG = 255;
+    _indexB = 255;
+    _opacity = 1;
+  }
+
+  void setColorState(PointerEvent e) => setState(() {
+        final random = Random();
+        _indexR = random.nextInt(255);
+        _indexG = random.nextInt(255);
+        _indexB = random.nextInt(255);
+        _opacity = random.nextDouble();
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Listener(
+          onPointerDown: setColorState,
+          child: Scaffold(
+            body: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Hey there',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40,
+                            color: Color.fromRGBO(255 - _indexR, 255 - _indexG,
+                                255 - _indexB, 1))),
+                  ]),
+            ),
+            backgroundColor:
+                Color.fromRGBO(_indexR, _indexG, _indexB, _opacity),
+          )),
+    );
+  }
+}
